feb4bb830e2285aa5fa7be7dbfdec1c05d7ea782
Evgen904 <evgen904@gmail.com>
2020-04-13 19:22:44 +0300

    Отображение состояния ответа

--- src/containers/Quiz/Quiz.js
+++ src/containers/Quiz/Quiz.js
@@ -6,6 +6,7 @@ class Quiz extends Component {
 
   state = {
     activeQuestion: 0,
+    answerState: null,
     quiz: [
       {
         id: 1,
@@ -33,10 +34,41 @@ class Quiz extends Component {
   }
 
   onAnswerClickHandler = answerId => {
-    console.log('Answer Id: ', answerId);
-    this.setState({
-      activeQuestion: this.state.activeQuestion + 1
-    })
+    const question = this.state.quiz[this.state.activeQuestion]
+
+    if (question.rightAnswerId === answerId) {
+
+      this.setState({
+        answerState: {
+          [answerId]: 'success'
+        }
+      })
+
+      const timeout = window.setTimeout(() => {
+        if (this.isQuizFinished()) {
+          console.log('Finished');
+        } else {
+          this.setState({
+            activeQuestion: this.state.activeQuestion + 1,
+            answerState: null
+          })
+        }
+        window.clearTimeout(timeout)
+      },1000)
+
+    } else {
+
+      this.setState({
+        answerState: {
+          [answerId]: 'error'
+        }
+      })
+
+    }
+  }
+
+  isQuizFinished() {
+    return this.state.activeQuestion + 1 === this.state.quiz.length
   }
 
   render() {
@@ -50,6 +82,7 @@ class Quiz extends Component {
             onAnswerClick={this.onAnswerClickHandler}
             quizLength={this.state.quiz.length}
             answerNumber={this.state.activeQuestion + 1}
+            state={this.state.answerState}
           />
         </div>
       </div>
