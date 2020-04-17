89cebc3404825b7f67043f91235da331f42aaee2
Evgen904 <evgen904@gmail.com>
2020-04-17 22:44:42 +0300

    Валидация. Часть 1

--- src/containers/Auth/Auth.js
+++ src/containers/Auth/Auth.js
@@ -5,6 +5,35 @@ import Input from '../../components/UI/Input/Input'
 
 export default class Auth extends Component {
 
+  state = {
+    formControls: {
+      email: {
+        value: '',
+        type: 'email',
+        label: 'Email',
+        errorMeaasage: 'Введите корректный email',
+        valid: false,
+        touched: false,
+        validation: {
+          required: true,
+          email: true
+        }
+      },
+      password: {
+        value: '',
+        type: 'password',
+        label: 'Пароль',
+        errorMeaasage: 'Введите корректный Пароль',
+        valid: false,
+        touched: false,
+        validation: {
+          required: true,
+          minLength: 6
+        }
+      }
+    }
+  }
+
   loginHandler = () => {
 
   }
@@ -17,19 +46,37 @@ export default class Auth extends Component {
 
   }
 
+  onChangeHandler = (event, controlName) => {
+    console.log(`${controlName}: `, event.target.value);
+  }
+
+  renderInputs() {
+    return Object.keys(this.state.formControls).map((controlName, index) => {
+      const control = this.state.formControls[controlName]
+      return (
+        <Input
+          key={controlName + index}
+          type={control.type}
+          value={control.value}
+          valid={control.valid}
+          touched={control.touched}
+          label={control.label}
+          shouldValidate={!!control.validation}
+          errorMessage={control.errorMessage}
+          onChange={event => this.onChangeHandler(event, controlName)}
+        />
+      )
+    })
+  }
+
   render() {
     return(
       <div className={classes.Auth}>
         <div>
           <h1>Авторизация</h1>
           <form className={classes.AuthForm} onSubmit={this.submitHandler}>
-            <Input
-              label="Email"
-            />
 
-            <Input
-              label="Пароль"
-            />
+            { this.renderInputs() }
 
             <Button
               type={'success'}
