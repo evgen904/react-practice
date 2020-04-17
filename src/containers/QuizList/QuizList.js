01fecb5457298ea766843c26fb141bb0401b897f
Evgen904 <evgen904@gmail.com>
2020-04-17 19:48:25 +0300

    Навигация

--- src/containers/QuizList/QuizList.js
+++ src/containers/QuizList/QuizList.js
@@ -3,9 +3,9 @@ import React, {Component} from "react";
 export default class QuizList extends Component {
   render() {
     return(
-        <div>
-          <h1>Quiz List</h1>
-        </div>
+      <div>
+        <h1>Quiz List</h1>
+      </div>
     )
   }
 }
