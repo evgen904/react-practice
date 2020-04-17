226772506a5520fa3330e2a64891a0c0b7215ec3
Evgen904 <evgen904@gmail.com>
2020-04-17 21:35:48 +0300

    Страница авторизации - Форма

--- src/containers/Auth/Auth.js
+++ src/containers/Auth/Auth.js
@@ -1,10 +1,43 @@
 import React, {Component} from "react";
+import classes from './Auth.module.scss'
+import Button from '../../components/UI/Button/Button'
 
 export default class Auth extends Component {
+
+  loginHandler = () => {
+
+  }
+
+  registrationHandler = () => {
+
+  }
+
+  submitHandler = () => {
+
+  }
+
   render() {
     return(
-      <div>
-        <h1>Auth</h1>
+      <div className={classes.Auth}>
+        <div>
+          <h1>Авторизация</h1>
+          <form className={classes.AuthForm} onSubmit={this.submitHandler}>
+            <input type="text"/>
+            <input type="text"/>
+            <Button
+              type={'success'}
+              onClick={this.loginHandler}
+            >
+              Войти
+            </Button>
+            <Button
+              type={'primary'}
+              onClick={this.registrationHandler}
+            >
+              Зарегистрироваться
+            </Button>
+          </form>
+        </div>
       </div>
     )
   }
