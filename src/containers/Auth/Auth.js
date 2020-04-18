26a26f0a01e6c59afedb8c7d1bfae25e13914d52
evgen904 <evgen904@gmail.com>
2020-04-18 20:44:07 +0300

    Валидация формы

--- src/containers/Auth/Auth.js
+++ src/containers/Auth/Auth.js
@@ -7,6 +7,7 @@ import is from 'is_js'
 export default class Auth extends Component {
 
   state = {
+    isFormValid: false,
     formControls: {
       email: {
         value: '',
@@ -79,8 +80,13 @@ export default class Auth extends Component {
 
     formControls[controlName] = control
 
+    let isFormValid = true
+    Object.keys(formControls).forEach(name => {
+      isFormValid = formControls[name].valid && formControls
+    })
+
     this.setState({
-      formControls
+      formControls, isFormValid
     })
   }
 
@@ -115,12 +121,14 @@ export default class Auth extends Component {
             <Button
               type={'success'}
               onClick={this.loginHandler}
+              disabled={!this.state.isFormValid}
             >
               Войти
             </Button>
             <Button
               type={'primary'}
               onClick={this.registrationHandler}
+              disabled={!this.state.isFormValid}
             >
               Зарегистрироваться
             </Button>
