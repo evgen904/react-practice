2cb7cbf04f6ec947475ee2d6d918826a9abaceae
evgen904 <evgen904@gmail.com>
2020-04-18 22:32:49 +0300

    Создание компонента Select

--- src/containers/QuizCreator/QuizCreator.js
+++ src/containers/QuizCreator/QuizCreator.js
@@ -2,6 +2,7 @@ import React, {Component} from "react";
 import classes from './QuizCreator.module.scss'
 import Button from '../../components/UI/Button/Button'
 import Input from '../../components/UI/Input/Input'
+import Select from '../../components/UI/Select/Select'
 import Auxiliary from '../../hoc/Auxiliary/Auxiliary'
 import {createControl} from '../../form/formFramework'
 
@@ -31,7 +32,8 @@ export default class QuizCreator extends Component {
 
   state = {
     quiz: [],
-    formControls: createFormControls()
+    formControls: createFormControls(),
+    rightAnswerId: 1
   }
 
   submitHandler = event => {
@@ -71,7 +73,26 @@ export default class QuizCreator extends Component {
     })
   }
 
+  selectChangeHandler = event => {
+    this.setState({
+      rightAnswerId: event.target.value
+    })
+  }
+
   render() {
+
+    const select = <Select
+      label="Выберите правильный ответ"
+      value={this.state.rightAnswerId}
+      onChange={this.selectChangeHandler}
+      options={[
+        {text: '1', value: 1},
+        {text: '2', value: 2},
+        {text: '3', value: 3},
+        {text: '4', value: 4}
+      ]}
+    />
+
     return(
       <div className={classes.QuizCreator}>
         <div>
@@ -81,7 +102,7 @@ export default class QuizCreator extends Component {
 
             { this.renderControls() }
 
-            <select></select>
+            { select }
 
             <Button
               type={'primary'}
