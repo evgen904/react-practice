c95f3b0e8866fae44c2ab6e597cc57d63e4a8731
evgen904 <evgen904@gmail.com>
2020-04-20 02:42:05 +0300

    Авторизация

--- src/containers/Auth/Auth.js
+++ src/containers/Auth/Auth.js
@@ -3,6 +3,7 @@ import classes from './Auth.module.scss'
 import Button from '../../components/UI/Button/Button'
 import Input from '../../components/UI/Input/Input'
 import is from 'is_js'
+import axios from 'axios'
 
 export default class Auth extends Component {
 
@@ -36,16 +37,38 @@ export default class Auth extends Component {
     }
   }
 
-  loginHandler = () => {
+  loginHandler = async () => {
+    const authData = {
+      email: this.state.formControls.email.value,
+      password: this.state.formControls.password.value,
+      returnSecureToken: true
+    }
+    try {
+      const response = await axios.post('https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyBTs7AM3rZ4FzzTdhC46uGQA8m9PywiCxs', authData)
 
+      console.log(response.data);
+    } catch (e) {
+      console.log(e);
+    }
   }
 
-  registrationHandler = () => {
+  registerHandler = async () => {
+    const authData = {
+      email: this.state.formControls.email.value,
+      password: this.state.formControls.password.value,
+      returnSecureToken: true
+    }
+    try {
+      const response = await axios.post('https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyBTs7AM3rZ4FzzTdhC46uGQA8m9PywiCxs', authData)
 
+      console.log(response.data);
+    } catch (e) {
+      console.log(e);
+    }
   }
 
-  submitHandler = () => {
-
+  submitHandler = (event) => {
+    event.preventDefault()
   }
 
   validateControl(value, validation) {
@@ -127,7 +150,7 @@ export default class Auth extends Component {
             </Button>
             <Button
               type={'primary'}
-              onClick={this.registrationHandler}
+              onClick={this.registerHandler}
               disabled={!this.state.isFormValid}
             >
               Зарегистрироваться
