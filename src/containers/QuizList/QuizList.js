3a2bc5588ca34609f96d3d26aab9e7d790a282d6
evgen904 <evgen904@gmail.com>
2020-04-20 01:13:34 +0300

    Работа с сервером - Настройка проекта

--- src/containers/QuizList/QuizList.js
+++ src/containers/QuizList/QuizList.js
@@ -1,8 +1,15 @@
 import React, {Component} from "react";
 import classes from './QuizList.module.scss'
 import {NavLink} from "react-router-dom";
+import axios from 'axios'
 
 export default class QuizList extends Component {
+
+  componentDidMount() {
+    axios.get('https://react-quiz-72735.firebaseio.com/quiz.json').then(response => {
+      console.log(response);
+    })
+  }
 
   renderQuizes() {
     return [1,2,3].map((quiz, index) => {
