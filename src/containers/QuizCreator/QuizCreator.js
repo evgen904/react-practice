bc6d79496fe145c04e5224940b50509e445a8561
evgen904 <evgen904@gmail.com>
2020-04-19 23:29:20 +0300

    Добавление вопроса

--- src/containers/QuizCreator/QuizCreator.js
+++ src/containers/QuizCreator/QuizCreator.js
@@ -43,10 +43,39 @@ export default class QuizCreator extends Component {
 
   addQuestionHandler = (event) => {
     event.preventDefault()
+
+    const quiz = this.state.quiz.concat()
+    const index = this.state.quiz.length + 1
+
+    const {question, option1, option2, option3, option4} = this.state.formControls
+
+    const questionItem = {
+      question: question.value,
+      id: index,
+      rightAnswerId: this.state.rightAnswerId,
+      answers: [
+        {text: option1.value, id: option1.id},
+        {text: option2.value, id: option2.id},
+        {text: option3.value, id: option3.id},
+        {text: option4.value, id: option4.id}
+      ]
+    }
+
+    quiz.push(questionItem)
+
+    this.setState({
+      quiz,
+      formControls: createFormControls(),
+      rightAnswerId: 1,
+      isFormValid: false
+    })
   }
 
-  createQuizHendler = () => {
+  createQuizHendler = (event) => {
+    event.preventDefault()
 
+    console.log(this.state.quiz);
+    // TODO: Server
   }
 
   changeHandler = (value, controlName) => {
@@ -127,7 +156,7 @@ export default class QuizCreator extends Component {
             <Button
               type={'success'}
               onClick={this.createQuizHendler}
-              disabled={this.state.quiz === 0}
+              disabled={this.state.quiz.length === 0}
             >
               Создать тест
             </Button>
